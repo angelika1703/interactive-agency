07257cb72edf7cf2a69999b8affd04d5cfaab81d
Angelika <kors-anzhelika@yandex.ru>
2018-07-23 19:04:16 +0300

    finished

--- js/main.js
+++ js/main.js
@@ -22,4 +22,18 @@ $(document).ready(function(){
             }
         ]
     });
+    $('a[href^="#"]').click(function(e){
+        e.preventDefault();
+        var el = $(this).attr('href');
+        var destination = $(el).offset().top;
+        $('body, html').animate({scrollTop: destination}, 1000);
+    });
 });
+function myMap() {
+    var mapCanvas = document.getElementById("map");
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.2),
+        zoom: 10
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+}
